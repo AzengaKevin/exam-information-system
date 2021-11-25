0081a13eeed9c4a3561d16ed1e2c2fd9641459b5
Azenga Kevin <azenga.kevin7@gmail.com>
2021-11-25 22:38:26 +0300

    WIP: Testing role and permission persistence

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
     use HasFactory,SoftDeletes;
 
     protected $guarded = [];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+
+        $this->attributes['slug'] = Str::slug($value);
+        
+    }
 }
---
+++ tests/Feature/PermissionPersistenceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Permission;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PermissionPersistenceTest extends TestCase
+{
+    
+    use RefreshDatabase, WithFaker;
+    use RefreshDatabase, WithFaker;
+
+    /** @group roles */
+    public function testAPermissionCanBePersistedToTheDatabase()
+    {
+        $payload = [
+            'name' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph()
+        ];
+
+        $permission = Permission::create($payload);
+
+        $this->assertEquals($payload['name'], $permission->name);
+        $this->assertEquals($payload['description'], $permission->description);
+        
+    }
+}
