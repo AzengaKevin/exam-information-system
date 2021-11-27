44fafe044f561b860b26d2b30a2380d3b8545052
Azenga Kevin <azenga.kevin7@gmail.com>
2021-11-27 19:14:38 +0300

    FEAT: Updating students

--- app/Http/Livewire/Students.php
+++ app/Http/Livewire/Students.php
@@ -94,4 +94,56 @@ class Students extends Component
             
         }
     }
+
+    public function editStudent(Student $student)
+    {
+        $this->studentId = $student->id;
+
+        $this->adm_no = $student->adm_no;
+        $this->upi = $student->upi;
+        $this->name = $student->name;
+        $this->dob = $student->dob->format('Y-m-d');
+        $this->gender = $student->gender;
+        $this->stream_id = $student->stream_id;
+        $this->admission_level_id = $student->admission_level_id;
+        $this->description = $student->description;
+
+        $this->emit('show-upsert-student-modal');
+    }
+
+    public function updateStudent()
+    {
+        $data = $this->validate();
+
+        try {
+
+            /** @var Student */
+            $student = Student::findOrFail($this->studentId);
+
+            if($student->update($data)){
+
+                $this->reset();
+
+                $this->resetValidation();
+
+                session()->flash('status', 'Student has been successfully updated');
+
+                $this->emit('hide-upsert-student-modal');
+
+            }
+
+
+        } catch (\Exception $exception) {
+
+            Log::error($exception->getMessage(), [
+                'action' => __METHOD__,
+                'student-id' => $this->studentId
+            ]);
+
+            session()->flash('error', 'Fatal error occurred while udating student');
+
+            $this->emit('hide-upsert-student-modal');
+        }
+        
+    }
 }
--- tests/Feature/StudentsManagementTest.php
+++ tests/Feature/StudentsManagementTest.php
@@ -70,4 +70,31 @@ class StudentsManagementTest extends TestCase
         $this->assertEquals($payload['level_id'], $student->level_id);
         
     }
+
+    /** @group students */
+    public function testAuthorizedUserCanUpdateAStudent()
+    {
+        $this->withoutExceptionHandling();
+
+        $student = Student::factory()->create();
+
+        $payload = Student::factory()->make()->toArray();
+
+        Livewire::test(Students::class)
+            ->call('editStudent', $student)
+            ->set('adm_no', $payload['adm_no'])
+            ->set('name', $payload['name'])
+            ->set('gender', $payload['gender'])
+            ->set('dob', $payload['dob'])
+            ->set('admission_level_id', $payload['admission_level_id'])
+            ->set('stream_id', $payload['stream_id'])
+            ->set('description', $payload['description'])
+            ->call('updateStudent');
+
+        $this->assertEquals($payload['adm_no'], $student->fresh()->adm_no);
+        $this->assertEquals($payload['name'], $student->fresh()->name);
+        $this->assertEquals($payload['gender'], $student->fresh()->gender);
+        $this->assertEquals($payload['admission_level_id'], $student->fresh()->admission_level_id);
+        $this->assertEquals($payload['level_id'], $student->fresh()->level_id);
+    }
 }
