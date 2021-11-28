fd42c763c5738e862145f67b7013650a1da93578
Azenga Kevin <azenga.kevin7@gmail.com>
2021-11-28 12:56:21 +0300

    WIP: Testing Departments setup

---
+++ tests/Feature/DepartmentPersistenceTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Department;
+use Illuminate\Support\Str;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DepartmentPersistenceTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function testADepartmentCanBePersistedToTheDatabase()
+    {
+        $payload = Department::factory()->make()->toArray();
+
+        $department = Department::create($payload);
+
+        $this->assertNotNull($department);
+
+        $this->assertEquals($payload['name'], $department->name);
+        $this->assertEquals($payload['description'], $department->description);
+        $this->assertEquals( Str::slug($payload['name']), $department->slug);
+    }
+}
