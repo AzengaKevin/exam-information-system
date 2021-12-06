69a03fcf448cc4995b9db30c20d2dfe34bf38585
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-06 13:44:14 +0300

    FEAT: Authorization of uploading scores, only teachers are allowed access to that

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        
+        Gate::define('access-upload-scores-page', function(User $user){
+            return $user->authenticatable_type == 'teacher'
+                ? Response::allow()
+                : Response::deny('Only teachers can access the upload scores page');
+        });
     }
 }
