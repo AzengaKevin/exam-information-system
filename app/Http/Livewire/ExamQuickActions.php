f65cd16161fb93913f6440285883aa9867bb75d5
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-07 19:33:18 +0300

    WIP: Changing exam status

--- app/Http/Livewire/ExamQuickActions.php
+++ app/Http/Livewire/ExamQuickActions.php
@@ -5,20 +5,28 @@ namespace App\Http\Livewire;
 use App\Actions\Exam\CreateScoresTable;
 use App\Models\Exam;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class ExamQuickActions extends Component
 {
+    /** @var Exam */
     public $exam;
+
+    public $status;
 
     public function mount(Exam $exam)
     {
         $this->exam = $exam;
+
+        $this->status = $exam->status;
     }
 
     public function render()
     {
-        return view('livewire.exam-quick-actions');
+        return view('livewire.exam-quick-actions', [
+            'statuses' => Exam::examStatusOptions()
+        ]);
     }
 
     public function createScoresTable()
@@ -36,6 +44,40 @@ class ExamQuickActions extends Component
             ]);
             
             session()->flash('error', "Failed creating {$this->exam->name} scores table");
+        }
+        
+    }
+
+    public function changeExamStatus()
+    {
+        $data = $this->validate(['status' => ['bail', 'required', Rule::in(Exam::examStatusOptions())]]);
+
+        try {
+
+            if($this->exam->update($data)){
+
+                if ($this->exam->fresh()->isInMarking()) {
+
+                    CreateScoresTable::invoke($this->exam);
+
+                }
+
+                session()->flash('status', 'Exam status successfully changed');
+
+                $this->emit('hide-change-exam-status-modal');
+            }
+
+
+        } catch (\Exception $exception) {
+
+            Log::error($exception->getMessage(), [
+                'action' => __METHOD__
+            ]);
+            
+            session()->flash('error', "Failed changing {$this->exam->name} status");
+
+            $this->emit('hide-change-exam-status-modal');
+            
         }
         
     }
--- resources/views/livewire/exam-quick-actions.blade.php
+++ resources/views/livewire/exam-quick-actions.blade.php
@@ -3,9 +3,22 @@
         <h5>Exam Quick Actions</h5>
         <hr>
         <x-feedback />
-        <button wire:click="createScoresTable" class="btn btn-lg d-block w-100 btn-primary hstack g-5">
-            <i class="fa fa-plus"></i>
-            <span>Scores Table</span>
-        </button>
+
+        @can('change-exam-status')
+        <div>
+            <button wire:click="createScoresTable" class="btn btn-primary hstack gap-2">
+                <i class="fa fa-plus"></i>
+                <span>Scores Table</span>
+            </button>
+        </div>
+        <div class="mt-3">
+            <button data-bs-toggle="modal" data-bs-target="#change-status-exam-modal"
+                class="btn d-block btn-primary hstack gap-2">
+                <i class="fa fa-pencil-alt"></i>
+                <span>Change Status</span>
+            </button>
+        </div>
+        <x-modals.exams.change-status :name="$exam->name" :statuses="$statuses" />
+        @endcan
     </div>
 </div>
