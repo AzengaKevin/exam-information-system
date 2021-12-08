980226e91b397cfe35fdfe8dfff70f1da765c0ea
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-08 11:40:17 +0300

    FEAT: Level Units Exam Scores Aggregate Generation

---
+++ resources/views/livewire/level-unit-exam-scores.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <x-feedback />
+
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead class="text-uppercase">
+                @foreach ($cols as $col)
+                <th>{{ $col }}</th>
+                @endforeach
+                <th>Actions</th>
+            </thead>
+            <tbody>
+                @if ($data->count())
+                @foreach ($data as $item)
+                <tr>
+                    @foreach ($cols as $col)
+                    @if (in_array($col, $subjectCols) || $col == "average")
+                    <td>{{ optional(json_decode($item->$col))->score ?? null }}{{ optional(json_decode($item->$col))->grade ?? null }}
+                    </td>
+                    @else
+                    <td>{{ $item->$col }}</td>
+                    @endif
+                    @endforeach
+                    <td>
+                        <button wire:click="showGenerateAggregatesModal({{ $item->admno }})" class="btn btn-sm btn-outline-primary hstack gap-2">
+                            <i class="fa fa-calculator"></i>
+                            <span>Aggregates</span>
+                        </button>
+                    </td>
+                </tr>
+                @endforeach
+                @else
+                <tr>
+                    <td colspan="{{ count($cols) + 1 }}">No data found yet</td>
+                </tr>
+                @endif
+            </tbody>
+        </table>
+    </div>
+
+    <x-modals.exams.scores.generate-aggregates :admno="$admno" :levelUnit="$levelUnit" />
+</div>
