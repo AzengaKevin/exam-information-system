3f079139df12dd2278d4d7c4691684d468f2796e
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-10 22:25:44 +0300

    WIP: Partial responsivenes fix

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -4,15 +4,15 @@
 
 @section('content')
 
-<div class="d-flex justify-content-between align-items-center">
+<div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
     <nav aria-label="breadcrumb">
-        <ol class="breadcrumb mb-0">
+        <ol class="breadcrumb mb-md-0">
             <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
             <li class="breadcrumb-item"><a href="{{ route('roles.index') }}">Authorization</a></li>
             <li class="breadcrumb-item active" aria-current="page">Permissions</li>
         </ol>
     </nav>
-    <button data-bs-toggle="modal" data-bs-target="#upsert-permission-modal" class="btn btn-outline-primary hstack gap-2 align-items-center">
+    <button data-bs-toggle="modal" data-bs-target="#upsert-permission-modal" class="btn btn-outline-primary d-inline-flex gap-2 align-items-center">
         <i class="fa fa-plus"></i>
         <span>Permission</span>
     </button>
