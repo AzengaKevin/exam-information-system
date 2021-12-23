d472e42db9f6738d880bd6e53f5ee5f4c48b8e05
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-23 21:22:30 +0300

    FEAT: Testing and providing a way to update general settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,30 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdateSettingsRequest;
+use App\Settings\GeneralSettings;
 use App\Settings\SystemSettings;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
     
-    public function index(Request $request, SystemSettings $systemSettings)
+    public function index(Request $request, SystemSettings $systemSettings, GeneralSettings $generalSettings)
     {
-        return view('settings.index', compact('systemSettings'));
+        return view('settings.index', compact('systemSettings', 'generalSettings'));
     }
 
-    public function update(UpdateSettingsRequest $updateSettingsRequest, SystemSettings $systemSettings)
+    public function update(UpdateSettingsRequest $updateSettingsRequest, SystemSettings $systemSettings, GeneralSettings $generalSettings)
     {
         $data = $updateSettingsRequest->validated();
         
-        $data = array_filter($data, fn($value, $key) => !is_null($value), ARRAY_FILTER_USE_BOTH);
+        foreach ($data['system'] as $key => $value) {
 
-        foreach ($data as $key => $value) {
-            $systemSettings->$key = $value;
+            if ($key == 'school_has_streams') {
+
+                $systemSettings->$key = boolval($value);
+
+            }else{
+
+                $systemSettings->$key = $value;
+
+            }
+
+        }
+        
+        if (!array_key_exists('school_has_streams', $data['system'])) {
+            $systemSettings->school_has_streams = false;
         }
 
+        $systemSettings->save();
+
+        
+        foreach ($data['general'] as $key => $value) {
+
+            $generalSettings->$key = $value;
+        }
+        
+        $generalSettings->save();
+        
         session()->flash('status', 'Settings has been successfully updated');
 
-        $systemSettings->save();
 
         return redirect()->back();
         
