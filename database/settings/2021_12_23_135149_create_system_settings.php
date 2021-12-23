f509aa80ea54a6c47040003a2559a2c192e81b3c
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-23 17:33:38 +0300

    FEAT: Setting up site setting resources using spatie/laravel settings package

---
+++ database/settings/2021_12_23_135149_create_system_settings.php
@@ -0,0 +1,18 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsBlueprint;
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+class CreateSystemSettings extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->inGroup('system', function(SettingsBlueprint $blueprint) : void{
+            $blueprint->add('school_name', 'Kisumu Boys High School');
+            $blueprint->add('school_type', 'boys');
+            $blueprint->add('school_level', 'secondary');
+            $blueprint->add('school_has_streams', true);
+        });
+
+    }
+}
