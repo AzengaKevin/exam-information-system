82a34ebe141b9da4975fd469906ec37a6a3bb5a9
Azenga Kevin <azenga.kevin7@gmail.com>
2021-12-28 14:21:01 +0300

    FIX: Add the extra button to level unit exam mgt, also change the design a little bit

--- resources/views/livewire/level-unit-exam-scores.blade.php
+++ resources/views/livewire/level-unit-exam-scores.blade.php
@@ -1,41 +1,45 @@
 <div>
     <x-feedback />
 
-    <div class="table-responsive">
-        <table class="table table-hover">
-            <thead class="text-uppercase">
-                @foreach ($cols as $col)
-                <th>{{ $col }}</th>
-                @endforeach
-                <th>Actions</th>
-            </thead>
-            <tbody>
-                @if ($data->count())
-                @foreach ($data as $item)
-                <tr>
-                    @foreach ($cols as $col)
-                    @if (in_array($col, $subjectCols))
-                    <td>{{ optional(json_decode($item->$col))->score ?? null }}{{ optional(json_decode($item->$col))->grade ?? null }}
-                    </td>
-                    @else
-                    <td>{{ $item->$col }}</td>
-                    @endif
-                    @endforeach
-                    <td>
-                        <button wire:click="showGenerateAggregatesModal({{ $item->admno }})" class="btn btn-sm btn-outline-primary hstack gap-2">
-                            <i class="fa fa-calculator"></i>
-                            <span>Aggregates</span>
-                        </button>
-                    </td>
-                </tr>
-                @endforeach
-                @else
-                <tr>
-                    <td colspan="{{ count($cols) + 1 }}">No data found yet</td>
-                </tr>
-                @endif
-            </tbody>
-        </table>
+    <div class="card mt-3">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead class="text-uppercase">
+                        @foreach ($cols as $col)
+                        <th>{{ $col }}</th>
+                        @endforeach
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        @if ($data->count())
+                        @foreach ($data as $item)
+                        <tr>
+                            @foreach ($cols as $col)
+                            @if (in_array($col, $subjectCols))
+                            <td>{{ optional(json_decode($item->$col))->score ?? null }}{{ optional(json_decode($item->$col))->grade ?? null }}
+                            </td>
+                            @else
+                            <td>{{ $item->$col }}</td>
+                            @endif
+                            @endforeach
+                            <td>
+                                <button wire:click="showGenerateAggregatesModal({{ $item->admno }})" class="btn btn-sm btn-outline-primary hstack gap-2">
+                                    <i class="fa fa-calculator"></i>
+                                    <span>Aggregates</span>
+                                </button>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @else
+                        <tr>
+                            <td colspan="{{ count($cols) + 1 }}">No data found yet</td>
+                        </tr>
+                        @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     <x-modals.exams.scores.level-units.generate-aggregates :admno="$admno" :levelUnit="$levelUnit" />
--- resources/views/partials/exams/scores/manage/class.blade.php
+++ resources/views/partials/exams/scores/manage/class.blade.php
@@ -1,1 +1,22 @@
-<livewire:level-unit-exam-scores :exam="$exam" :levelUnit="$levelUnit" />
+<div class="container-fluid">
+    <div class="d-flex gap-2 align-items-md-center flex-wrap">
+        <a href="#" data-bs-toggle="modal" data-bs-target="#generate-scores-aggregates-modal" role="button"
+            class="btn btn-primary d-inline-flex gap-2 align-items-center">
+            <i class="fa fa-calculator"></i>
+            <span>Aggregates</span>
+        </a>
+        <a href="#" data-bs-toggle="modal" data-bs-target="#publish-class-scores-modal" role="button"
+            class="btn btn-primary d-inline-flex gap-2 align-items-center">
+            <i class="fa fa-upload"></i>
+            <span>Publish</span>
+        </a>
+        <a href="#" data-bs-toggle="modal" data-bs-target="#rank-class-modal" role="button"
+            class="btn btn-primary d-inline-flex gap-2 align-items-center">
+            <i class="fa fa-sort-amount-down"></i>
+            <span>Rank</span>
+        </a>
+    </div>
+
+    <livewire:level-unit-exam-scores :exam="$exam" :levelUnit="$levelUnit" />
+
+</div>
