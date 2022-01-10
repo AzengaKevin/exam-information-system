1770d39563543a08d997ec81b087be7925ed91d2
Azenga Kevin <azenga.kevin7@gmail.com>
2022-01-10 13:10:04 +0300

    FEAT: User can now successfully update their own password

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,11 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row g-3 py-3">
-        <div class="col-md-6">
+        <div class="col-md-5">
+            <h5 class="text-dark">Profile Photo</h5>
+            <p class="text-muted">Your profile picture is used within this application, it makes identification much easier for other users.</p>
+        </div>
+        <div class="col-md-7">
             <livewire:user-profile-photo :user="$user" />
         </div>
-        <div class="col-md-6">
+        <hr class="my-3 my-md-5">
+        <div class="col-md-5">
+            <h5 class="text-dark">Profile Details</h5>
+            <p class="text-muted">Other than identification, some of the details in this section are essential for notification and instant communications. Make sure the details are verified where applicable.</p>
+        </div>
+        <div class="col-md-7">
             <livewire:user-profile-information :user="$user" />
+        </div>
+        <hr class="my-3 my-md-5">
+        <div class="col-md-5">
+            <h5 class="text-dark">Updated Password</h5>
+            <p class="text-muted">Ensure you are using a strong password to stay secure</p>
+        </div>
+        <div class="col-md-7">
+            <livewire:update-user-password :user="$user" />
         </div>
     </div>
 </div>
