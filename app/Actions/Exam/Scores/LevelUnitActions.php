7783ace3fdb62ea2d60141caa607d94fd635aeeb
Azenga Kevin <azenga.kevin7@gmail.com>
2022-01-13 11:26:27 +0300

    WIP: perform all level unit actions at once

---
+++ app/Actions/Exam/Scores/LevelUnitActions.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace App\Actions\Exam\Scores;
+
+use App\Models\Exam;
+use App\Models\Grade;
+use App\Models\Grading;
+use App\Models\LevelUnit;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class LevelUnitActions
+{
+    /**
+     * Generate exam aggregates for a level unit
+     * 
+     * @param Exam $exam
+     * @param LevelUNit $levelUnit
+     * 
+     */
+    public static function generateAggregates(Exam $exam, LevelUnit $levelUnit)
+    {
+        try {
+            
+            $cols = $exam->subjects->pluck("shortname")->toArray();
+
+            $tblName = Str::slug($exam->shortname);
+
+            /** @var Collection */
+            $data = DB::table($tblName)
+                ->where("level_unit_id", $levelUnit->id)
+                ->select(array_merge(["student_id"], $cols))->get();
+
+            $data->each(function($stuData) use($tblName, $cols){
+                $totalScore = 0;
+                $totalPoints = 0;
+                $populatedCols = 0;
+
+                foreach ($cols as $col) {
+
+                    if(!is_null($stuData->$col)){
+                        $populatedCols++;
+
+                        $subData = json_decode($stuData->$col);
+
+                        $totalScore += $subData->score ?? 0;
+                        $totalPoints += $subData->points ?? 0;
+                    }
+                }
+
+                $avgPoints = round($totalPoints / $populatedCols);
+                $avgScore = round($totalScore / $populatedCols);
+
+                $pgm = Grade::all(['points', 'grade'])->pluck('grade', 'points');
+
+                $avgGrade = $pgm[$avgPoints];
+
+                DB::table($tblName)
+                ->updateOrInsert([
+                    "student_id" => $stuData->student_id
+                ], [
+                    "mm" => $avgScore,
+                    "mg" => $avgGrade,
+                    'mp' => $avgPoints,
+                    'tp' => $totalPoints,
+                    'tm' => $totalScore
+                ]);
+            });
+
+        } catch (\Exception $exception) {
+            throw $exception;
+        }        
+    }
+
+    /**
+     * Generate ranks for a level-unit
+     * 
+     * @param Exam $exam
+     * @param LevelUnit $levelUnit
+     * 
+     */
+    public static function generateRanks(Exam $exam, LevelUnit $levelUnit, string $col = 'tm')
+    {
+        try {
+
+            $tblName = Str::slug($exam->shortname);
+
+            // Get order records from the databas with the admno number as the primary key
+            /** @var Collection */
+            $data = DB::table($tblName)
+                ->select(['student_id', $col])
+                ->where('level_unit_id', $levelUnit->id)
+                ->orderBy($col, 'desc')
+                ->get();
+
+            $data->each(function($item, $key) use($tblName){
+
+                $rank = $key + 1;
+
+                DB::table($tblName)->updateOrInsert(['student_id' => $item->student_id],['sp' => $rank]);
+                
+            });
+
+        } catch (\Exception $exception) {
+
+            throw $exception;
+
+        }
+        
+    }
+
+    /**
+     * Publish grade distribution got the specified level unit in the specified exam
+     * 
+     * @param Exam $exam
+     * @param LevelUnit $levelUnit
+     * 
+     */
+    public static function publishGradeDistribution(Exam $exam, LevelUnit $levelUnit)
+    {
+        
+        try {
+            
+            $tblName = Str::slug($exam->shortname);
+
+            /** @var Collection */
+            $data = DB::table($tblName)
+                ->where('level_unit_id', $levelUnit->id)
+                ->selectRaw("mg, COUNT(mg) AS grade_count")
+                ->distinct("mg")
+                ->groupBy('mg')
+                ->get()
+                ->pluck('grade_count', 'mg');
+
+            if ($data->count()) {
+
+                DB::beginTransaction();
+    
+                foreach (Grading::gradeOptions() as $grade) {
+
+                    DB::table('exam_level_unit_grade_distribution')
+                        ->updateOrInsert([
+                            'exam_id' => $exam->id,
+                            'level_unit_id' => $levelUnit->id,
+                            'grade' => $grade,
+                        ],['grade_count' => $data[$grade] ?? 0]);
+                }
+
+                DB::commit();
+    
+            }
+
+        } catch (\Exception $exception) {
+
+            DB::rollBack();
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * Publishing subject performance for the specified level unit in the specified exam
+     * 
+     * @param Exam $exam
+     * @param LevelUnit $levelUnit
+     * 
+     * @return bool
+     */
+    public static function publishSubjectPerformance(Exam $exam, LevelUnit $levelUnit) : bool
+    {
+
+        try {
+
+            $tblName = Str::slug($exam->shortname);
+
+            DB::beginTransaction();
+
+            $atLeastASubjectPublished = false;
+
+            foreach ($exam->subjects as $subject) {
+
+                $col = $subject->shortname;
+
+                $data = DB::table($tblName)
+                    ->selectRaw("AVG(JSON_UNQUOTE(JSON_EXTRACT($col, \"$.points\"))) AS avg_points, AVG(JSON_UNQUOTE(JSON_EXTRACT($col, \"$.score\"))) AS avg_score")
+                    ->where('level_unit_id', $levelUnit->id)
+                    ->whereNotNull($col)
+                    ->first();
+
+                if (!is_null($data->avg_points) && !is_null($data->avg_score)) {
+
+                    $atLeastASubjectPublished = true;
+                    
+                    $avgTotal = number_format($data->avg_score, 2);
+                    $avgPoints = number_format($data->avg_points, 4);
+    
+                    $pgm = Grade::all(['points', 'grade'])->pluck('grade', 'points');
+    
+                    $avgGrade = $pgm[intval(round($avgPoints))];
+    
+                    DB::table('exam_level_unit_subject_performance')
+                        ->updateOrInsert([
+                            'exam_id' => $exam->id,
+                            'level_unit_id' => $levelUnit->id,
+                            'subject_id' => $subject->id
+                        ], [
+                            'average' => $avgTotal,
+                            'points' => $avgPoints,
+                            'grade' => $avgGrade
+                        ]);
+                }
+
+            }
+
+            DB::commit();
+            
+            return $atLeastASubjectPublished;
+            
+        } catch (\Exception $exception) {
+
+            DB::rollBack();
+            
+            throw $exception;
+            
+        }
+        
+    }
+
+    /**
+     * Publish LevelUnit Exam Scores
+     * 
+     * @param Exam $exam
+     * @param LevelUnit $levelUnit
+     * 
+     */
+    public static function publishScores(Exam $exam, LevelUnit $levelUnit)
+    {        
+        try {
+
+            $tblName = Str::slug($exam->shortname);
+
+            $data = DB::table($tblName)
+                ->where("level_unit_id", $levelUnit->id)
+                ->selectRaw("AVG(tm) AS avg_total, AVG(mp) avg_points")
+                ->first();
+            
+            $avgTotal = number_format($data->avg_total, 2);
+            $avgPoints = number_format($data->avg_points, 4);
+
+            $pgm = Grade::all(['points', 'grade'])->pluck('grade', 'points');
+
+            $avgGrade = $pgm[intval(round($avgPoints))];
+
+            $exam->levelUnits()->syncWithoutDetaching([
+                $levelUnit->id => [
+                    "points" => $avgPoints,
+                    "grade" => $avgGrade,
+                    "average" => $avgTotal
+                ]
+            ]);
+
+        } catch (\Exception $exception) {
+            
+            throw $exception;
+
+        }
+        
+    }
+}
--- resources/views/partials/exams/scores/manage/class.blade.php
+++ resources/views/partials/exams/scores/manage/class.blade.php
@@ -1,26 +1,27 @@
 <div class="container-fluid">
     <div class="d-flex gap-2 align-items-md-center flex-wrap">
-        <a href="#" data-bs-toggle="modal" data-bs-target="#generate-scores-aggregates-modal" role="button"
-            class="btn btn-primary d-inline-flex gap-2 align-items-center">
-            <i class="fa fa-calculator"></i>
-            <span>Aggregates</span>
-        </a>
         <a href="#" data-bs-toggle="modal" data-bs-target="#publish-class-scores-modal" role="button"
             class="btn btn-primary d-inline-flex gap-2 align-items-center">
             <i class="fa fa-upload"></i>
             <span>Publish</span>
         </a>
+        <a href="#" data-bs-toggle="modal" data-bs-target="#generate-scores-aggregates-modal" role="button"
+            class="btn btn-primary d-inline-flex gap-2 align-items-center text-decoration-line-through">
+            <i class="fa fa-calculator"></i>
+            <span>Aggregates</span>
+        </a>
         <a href="#" data-bs-toggle="modal" data-bs-target="#rank-class-modal" role="button"
-            class="btn btn-primary d-inline-flex gap-2 align-items-center">
+            class="btn btn-primary d-inline-flex gap-2 align-items-center text-decoration-line-through">
             <i class="fa fa-sort-amount-down"></i>
             <span>Rank</span>
         </a>
         <a href="#" data-bs-toggle="modal" data-bs-target="#publish-level-unit-grade-dist-modal" role="button"
-            class="btn btn-primary d-inline-flex gap-2 align-items-center">
+            class="btn btn-primary d-inline-flex gap-2 align-items-center text-decoration-line-through">
             <i class="fa fa-upload"></i>
             <span>Publish Grade Dist.</span>
+        </a>
         <a href="#" data-bs-toggle="modal" data-bs-target="#publish-class-subjects-performance-modal" role="button"
-            class="btn btn-primary hstack gap-2">
+            class="btn btn-primary hstack gap-2 align-items-center text-decoration-line-through">
             <i class="fa fa-upload"></i>
             <span>Publish Subject Perf.</span>
         </a>
