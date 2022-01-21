ee663756bc9570d043ee367d21b02dec7dc8760e
Azenga Kevin <azenga.kevin7@gmail.com>
2022-01-21 18:23:00 +0300

    WIP: Guardian authorization and trashing logic

--- app/Http/Controllers/GuardiansController.php
+++ app/Http/Controllers/GuardiansController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guardian;
 use Illuminate\Http\Request;
 
 class GuardiansController extends Controller
 {
+
+    /**
+     * Creates Guardians Controller Instance
+     */
+    public function __construct() {
+
+        $this->middleware('auth');
+        
+        $this->authorizeResource(Guardian::class);
+
+    }
+
+    /**
+     * Show a list of all guardians
+     * 
+     * @param Request $request
+     * @return View
+     */
     public function index(Request $request)
     {
-        return view('guardians.index');
+        $trashed = $request->trashed;
+
+        if(boolval($trashed)) $this->authorize('viewTrashed', Guardian::class);
+
+        return view('guardians.index', compact('trashed'));
     }
 }
