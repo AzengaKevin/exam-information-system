8c793a895f7f4199fc3d6be2b4acbc0e4760bc26
Azenga Kevin <azenga.kevin7@gmail.com>
2022-01-21 18:24:47 +0300

    WIP: Complete authorization by protecting visibility and enhancing the view design

--- resources/views/guardians/index.blade.php
+++ resources/views/guardians/index.blade.php
@@ -4,21 +4,7 @@
 
 @section('content')
 
-<div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb mb-md-0">
-            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Guardians</li>
-        </ol>
-    </nav>
-    <button data-bs-toggle="modal" data-bs-target="#upsert-guardian-modal" class="btn btn-outline-primary d-inline-flex gap-2 align-items-center">
-        <i class="fa fa-plus"></i>
-        <span>Guardian</span>
-    </button>
-</div>
-<hr>
-
-<livewire:guardians />
+<livewire:guardians :trashed="$trashed" />
 
 @endsection
 
