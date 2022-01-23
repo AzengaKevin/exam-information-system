70ca1510f5d03fa066617be7df2bf71d0f85286c
Azenga Kevin <azenga.kevin7@gmail.com>
2022-01-23 13:17:51 +0300

    WIP: Hostel managent authorization, exception handling and trash logic implementation

--- app/Http/Controllers/HostelsController.php
+++ app/Http/Controllers/HostelsController.php
@@ -9,14 +9,31 @@ class HostelsController extends Controller
 {
     public function __construct()
     {
-        return $this->middleware(['auth']);
+        $this->middleware(['auth']);
+
+        $this->authorizeResource(Hostel::class);
+
     }
 
-    public function index()
+    /**
+     * Show a list of all hostels in the database
+     * 
+     * @return View
+     */
+    public function index(Request $request)
     {
-        return view('hostels.index');
+        $trashed = $request->trashed;
+
+        if(boolval($trashed)) $this->authorize('viewTrashed', Hostel::class);
+
+        return view('hostels.index', compact('trashed'));
     }
 
+    /**
+     * Show hostel details
+     * 
+     * @param Hostel $hostel
+     */
     public function show(Hostel $hostel)
     {
         return view('hostels.show',compact('hostel'));
